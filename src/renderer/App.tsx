f038521edc7202e10bbdf37b9dbd9a223b8cbc73
Meep <supermeip@gmail.com>
2023-01-17 18:26:59 -0500

    initializing from boilerplate electron react app

---
+++ src/renderer/App.tsx
@@ -0,0 +1,15 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import '../../node_modules/react-grid-layout/css/styles.css';
+import '../../node_modules/react-resizable/css/styles.css';
+import StartPage from '../components/pages/start';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+      </Routes>
+    </Router>
+  );
+}
